73dcbe3ea385ba5b1f96a60ec784d5d2a4984f2a
carlosrodrigues-12 <carlos_rodrigues@discente.ufg.br>
2021-04-17 11:34:51 -0300

    Using SODA with oracle, create new colletion documents

---
+++ oracle_soda_create_drop.js
@@ -0,0 +1,73 @@
+// ****************************************************************
+// *  @Autor -> Carlos Rodrigues                                  *
+// *  Para utilizar 'oracledb', instale o client da oracle        *
+// *                                                              *
+// *  Para utilizar SODA no banco oracle, dê permissão ao usuário *
+// *                                                              *
+// *  ex: GRANT SODA_APP TO carlos;                               *
+// ****************************************************************
+
+const oracledb = require('oracledb')
+// api de conexão com banco, oracle - node
+
+const mypw = "senha_banco";
+oracledb.autoCommit = true;
+
+async function run() {
+
+  let connection; // Var de conexão com banco
+
+  try { // Tenta fazer a conexão com banco
+    connection = await oracledb.getConnection(
+      {
+        user              : "carlos", // Usuario do banco
+        password          : mypw, // Senha do user
+        connectionString  : "192.168.56.3/xe" // Local e schema do banco
+      }
+    )
+
+    // Se tudo der certo - Print para validar a conexão
+    console.log("Conexão com banco realizada com sucesso\n")
+
+    // const soda = connection.getSodaDatabase();
+    // const collection = await soda.createCollection("mycollection");
+    // const indexSpec = { "name": "CITY_IDX",
+    //                     "fields": [{
+    //                       "path": "address.city",
+    //                       "datatype": "string",
+    //                       "order": "asc"
+    //                     }]
+    //                   }
+    // await collection.createIndex(indexSpec);
+
+    // const myContent = { name: "Sally", address: {city: "Melbourne"} };
+    // await myCollection.insertOne(myContent);
+    //
+    // const filterSpec = { "address.city": "Melbourne" };
+    // const myDocuments = await myCollection.find().filter(filterSpec).getDocuments();
+    // myDocuments.forEach(function(element){
+    //   const content = element.getContent();
+    //   console.log(content.name + ' lives in Melbourne.');
+    // })
+    console.log("Sucesso\n");
+
+    // Exclusao SODA
+
+    const soda = connection.getSodaDatabase();
+    // const collection = await soda.createCollection()
+    const collection = await sodaCollection.drop("nodb_soda_collection")
+
+  } catch (err) { // Caso não consiga, catch vai tratar a falha
+    console.error("Error in processing:\n", err)
+  } finally { // Finalização
+    if (connection){
+      try{
+        await connection.close()
+      } catch (err){
+        console.log("Error in closing connection:\n", err);
+      }
+    } // If para fechar a conexao com banco
+  }// Finally
+}
+
+run()
